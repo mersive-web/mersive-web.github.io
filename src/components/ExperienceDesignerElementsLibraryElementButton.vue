360006e3d15f6099f9a222c1b89e7c1f5a855509
frazard99 <francesco.sardaro99@gmail.com>
2025-04-17 11:33:10 +0200

    update ExperienceDesignerElementsLibraryElementButton

--- src/components/ExperienceDesignerElementsLibraryElementButton.vue
+++ src/components/ExperienceDesignerElementsLibraryElementButton.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
+import eventBus from '@/eventBus'
+import { experienceDesignerElementsLibrarySearchQueryIK } from '@/injectionKeys'
+import { computed, inject, ref } from 'vue'
+import infoIcon from '@/assets/icons/info_filled_300_48.svg'
+
 const props = defineProps({
-    imageFilePath: {
+    previewImageFilePath: {
         type: String,
         required: true,
     },
@@ -8,18 +13,71 @@ const props = defineProps({
         type: String,
         required: true,
     },
+    modelFilePath: {
+        type: String,
+        required: true,
+    },
+    modelPageUrl: {
+        type: String,
+        required: true,
+    },
 })
+
+const experienceDesignerElementsLibrarySearchQuery = inject(
+    experienceDesignerElementsLibrarySearchQueryIK,
+    ref(''),
+)
+
+const showButton = computed(() => {
+    if (experienceDesignerElementsLibrarySearchQuery.value != '') {
+        return props.elementName
+            .toLowerCase()
+            .includes(experienceDesignerElementsLibrarySearchQuery.value.toLowerCase().trim())
+    } else {
+        return true
+    }
+})
+
+function handleElementButtonClick() {
+    eventBus.emit('elementsLibraryElementButtonClicked', {
+        elementName: props.elementName,
+        elementPreviewImageFilePath: props.previewImageFilePath,
+        element3DModelFilePath: props.modelFilePath,
+    })
+}
+
+function handleInfoIconClick() {
+    window.open(props.modelPageUrl, '_blank')
+}
 </script>
 
 <template>
-    <button class="elements-library__element-button" tabindex="-1">
-        <img :src="imageFilePath" alt="" />
-        <span>{{ props.elementName }}</span>
+    <button
+        v-show="showButton"
+        class="elements-library__element-button"
+        tabindex="-1"
+        @click="handleElementButtonClick"
+    >
+        <img
+            class="elements-library__element-button__info-icon"
+            :src="infoIcon"
+            alt=""
+            title="Open credits page of this 3D model in another tab"
+            @click.stop="handleInfoIconClick"
+        />
+        <img
+            class="elements-library__element-button__preview-image"
+            :src="previewImageFilePath"
+            alt=""
+        />
+        <span class="elements-library__element-button__model-name">{{ props.elementName }}</span>
     </button>
 </template>
 
 <style scoped>
 .elements-library__element-button {
+    position: relative;
+
     width: 150px;
 
     display: flex;
@@ -29,7 +87,7 @@ const props = defineProps({
     font-size: 0.8em;
     text-align: center;
 
-    background-color: var(--mersive-c-grey);
+    background-color: var(--mersive-c-grey-darker);
     color: var(--mersive-c-white);
 
     border-radius: 6px;
@@ -39,15 +97,26 @@ const props = defineProps({
 }
 
 .elements-library__element-button:hover {
-    background-color: var(--mersive-c-grey-dark);
+    background-color: var(--mersive-c-blue-dark);
 }
 
-.elements-library__element-button img {
+.elements-library__element-button__info-icon {
+    position: absolute;
+    top: 4px;
+    right: 4px;
+
+    width: 24px;
+    height: 24px;
+
+    filter: drop-shadow(0px 2px 4px #00000090);
+}
+
+.elements-library__element-button__preview-image {
     width: 150px;
     height: 150px;
 }
 
-.elements-library__element-button span {
+.elements-library__element-button__model-name {
     margin: 0.2em 0.4em;
 }
 </style>
